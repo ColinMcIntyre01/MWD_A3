523041d1151adb5955e6f77b1662c28e7117f41a
Colin McIntyre <Colin McIntyre>
2025-03-10 12:00:40 -0400

    added the logic to the Deck File which will allow the cards to interact with the deck.

--- src/Components/Deck.js
+++ src/Components/Deck.js
@@ -4,10 +4,26 @@ import "./Deck.css";
 
 const Deck = ({Deck, drawnCards, onPick, pickedCard})=>{
 
+return(
+<div className="deck-container">
+ <div className="deck">
+ {deck.length > 0 ? "Deck" : "No cards remaining"}
+ </div>
+ <div className="drawn-cards">
+ {drawnCards.map((card, index) => (
+          <Card
+            key={index}
+            suit={card.suit}
+            value={card.value}
+            isPicked={pickedCard === card}
+            onClick={() => onPick(card)}
+          />
+        ))}
+ </div>
+</div>
+);
 
 
 
-
-    
-}
+};
 export default Deck;
