5e410d92a77bded4167c96e164a89e944b8b5523
Colin McIntyre <Colin McIntyre>
2025-03-10 13:00:29 -0400

    Created 2 const arrays representing the suits and rank of the cards in the app file.

--- src/App.js
+++ src/App.js
@@ -3,8 +3,17 @@ import Deck from "./components/Deck";
 import Controls from "./components/Controls";
 import './App.css';
 
+const suits = ["♥", "♦", "♣", "♠"];
+const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+
+const createDeck = () => {
+  return suits.flatMap((suit) => values.map((value) => ({ suit, value })));
+};
+
 function App() {
  
+
+
 }
 
 export default App;
