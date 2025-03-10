a978e468966a2a25edd9c99cc8c7629ee81005af
Colin McIntyre <Colin McIntyre>
2025-03-10 10:35:38 -0400

    added the card logic which will allow the cards to be made in html.

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,6 +1,18 @@
 import React from "react";
 import "./Card.css";
 
-const Card = ({ suit, value, isPicked, onClick }) => {}
+const Card = ({ suit, value, isPicked, onClick }) => {
+
+return(
+
+<div className={`card ${isPicked ? "picked" : ""}`} onClick={onClick}>
+
+   <span className="card-value">{value}</span>
+   <span className="card-suit">{suit}</span>
+
+</div>
+);
+
+};
 
 export default Card;
