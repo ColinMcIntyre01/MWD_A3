56b5a0dc3ff5ed49de3f7483ead8989a812ba5fc
Colin McIntyre <Colin McIntyre>
2025-03-10 10:16:39 -0400

    added the onClick logic to the buttons the controls file.

--- src/Components/Controls.js
+++ src/Components/Controls.js
@@ -3,7 +3,24 @@ import "./Controls.css";
 
 const Controls = ({ deal, reset, toss, regroup, addWildcard }) => {
 
+    return(
     
-}
+    <div className="controls">
+
+      <button onClick={() => deal(5)}>Deal 5</button>
+      <button onClick={() => deal(7)}>Deal 7</button>
+      <button onClick={reset}>Reset</button>
+      <button onClick={toss}>Toss</button>
+      <button onClick={regroup}>Regroup</button>
+      <button onClick={addWildcard}>Wildcard</button>
+
+
+
+    </div>
+
+
+    );
+
+};
 
 export default Controls;
