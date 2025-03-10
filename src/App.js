2b80b195033899956db5bfd41dad86411fe7da58
Colin McIntyre <Colin McIntyre>
2025-03-10 12:09:56 -0400

    Deleted the boiler plate code in the App.js file.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
+import Deck from "./components/Deck";
+import Controls from "./components/Controls";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code>ballsxzach.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ 
 }
 
 export default App;
